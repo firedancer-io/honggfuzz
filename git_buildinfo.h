09b97d476d3522944b4da3673391c2ca5f98fe2a
Charles Moyes <cmoyes@jumptrading.com>
2026-01-20 15:12:45 +0000

    log git info at startup

---
+++ git_buildinfo.h
@@ -0,0 +1,10 @@
+/* Auto-generated at build time - do not edit */
+#ifndef GIT_BUILDINFO_H
+#define GIT_BUILDINFO_H
+
+#define GIT_COMMIT_HASH "8193e32e"
+#define GIT_COMMIT_BRANCH "cmoyes/logrev"
+#define GIT_COMMIT_AUTHOR "cmoyes-jump"
+#define GIT_COMMIT_TITLE "Merge pull request #8 from firedancer-io/cmoyes/instrumentation"
+
+#endif /* GIT_BUILDINFO_H */
